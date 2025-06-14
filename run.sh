23a818c3edd20ad575e1b9f84187b122dc9267f6
Andrew Andreyev <andrew@andreyev.com.au>
2025-06-14 17:49:23 +1000

    Small changes to run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Simple script to run the Streamlit deduplicator app
 
-echo "Starting the Document Deduplicator..."
+echo "Starting the Document Deduplicator (via run.sh using 'command streamlit')..."
 # Secrets should be injected by the outer 'doppler run -- ./run.sh' call
-doppler run -- ./run.sh
+# Using 'command streamlit' to bypass any potential shell aliases or functions
+command streamlit run deduplicator.py --server.fileWatcherType none
+echo "run.sh: Streamlit command has been executed. If Streamlit started correctly, it will be running. If it exited, this script will now exit."
